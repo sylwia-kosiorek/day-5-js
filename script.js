75b3d7d52a0c102c932553ddf33a6be1812f38ec
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-28 10:04:14 +0200

    Html elements added

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 class Counter {
     constructor (){
         this.number = 0
+
+        this.render()
     }
 
     inc(){
         this.number = this.number + 1
+    }
+
+    render(){
+        const headerElement = document.createElement('h1') // utworzenie elementow w HTML
+        const btnElement = document.createElement('button')
+
+        headerElement.innerText = this.number 
+        btnElement.innerText = '+'
+
+        document.body.appendChild(headerElement)
+        document.body.appendChild(btnElement)
     }
 }
 
