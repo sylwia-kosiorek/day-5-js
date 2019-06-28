e8ea890261749ca4256d9e53880c99e4ac1d9c07
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-28 09:54:30 +0200

    Counters added

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 class Counter {
     constructor (){
-        this.number = 0 // cos jak zmienna dla klas ; )
+        this.number = 0
+    }
+
+    inc(){
+        this.number = this.number + 1
     }
 }
 
 const counter1 = new Counter()
+const counter2 = new Counter()
 
 console.log(counter1)
+console.log(counter2)
+
+console.log(counter1 === counter2)
