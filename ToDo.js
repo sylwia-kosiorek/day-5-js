e83dea1c24c8af42ee914c13d35f48a0f5ef0560
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-28 14:53:10 +0200

    Task app completed

--- ToDo.js
+++ ToDo.js
@@ -1,121 +1,145 @@
 class ToDo {
     constructor(selector) {
-      this.mainContainerElement = document.querySelector(selector)
-      this.uiContainer = null
-      this.tasksContainer = null
-  
-      this.isLoading = false
-      this.tasks = []
+        this.mainContainerElement = document.querySelector(selector)
+        this.uiContainer = null
+        this.tasksContainer = null
+
+        this.isLoading = false
+        this.tasks = []
     }
-  
+
     init() {
-      this.uiContainer = document.createElement('div')
-      this.tasksContainer = document.createElement('div')
-      this.mainContainerElement.appendChild(this.uiContainer)
-      this.mainContainerElement.appendChild(this.tasksContainer)
-  
-      this.loadFromDb()
-      this.render()
+        this.uiContainer = document.createElement('div')
+        this.tasksContainer = document.createElement('div')
+        this.mainContainerElement.appendChild(this.uiContainer)
+        this.mainContainerElement.appendChild(this.tasksContainer)
+
+        this.makeUI()
+        this.loadFromDb()
+        this.render()
     }
-  
+
+    makeUI() {
+        const input = document.createElement('input')
+        const btn = document.createElement('button')
+
+        btn.innerText = 'Dodaj zadanie'
+
+        btn.addEventListener(
+            'click',
+            () => {
+                this.addTask(input.value)
+                input.value = ''
+            }
+        )
+
+
+        this.uiContainer.appendChild(input)
+        this.uiContainer.appendChild(btn)
+    }
+
     setState(propName, newValue) {
-      this[propName] = newValue
-  
-      this.saveToDb()
-      this.render()
+        this[propName] = newValue
+
+        this.saveToDb()
+        this.render()
     }
-  
+
     loadFromDb() {
-      this.setState('isLoading', true)
-  
-      fetch('https://ad-snadbox.firebaseio.com/js5day2.json')
-        .then(response => response.json())
-        .then(value => {
-          this.setState('tasks', value || [])
-          this.setState('isLoading', false)
-        })
+        this.setState('isLoading', true)
+
+        fetch('https://js-baza.firebaseio.com/test.json')
+            .then(response => response.json())
+            .then(value => {
+                this.setState('tasks', value || [])
+                this.setState('isLoading', false)
+            })
     }
-  
+
     saveToDb() {
-      const data = JSON.stringify(this.tasks)
-  
-      fetch(
-        'https://ad-snadbox.firebaseio.com/js5day2.json',
-        {
-          method: 'PUT',
-          body: data
+        const data = JSON.stringify(this.tasks)
+
+        fetch(
+            'https://js-baza.firebaseio.com/test.json',
+            {
+                method: 'PUT',
+                body: data
+            }
+        )
+    }
+
+    addTask(newTaskName) {
+        if(!newTaskName) {
+            alert('uzupelnij nazwe zadania!!!')
+            return
         }
-      )
+        const newTasks = this.tasks.concat({
+            taskName: newTaskName,
+            isCompleted: false
+        })
+
+        this.setState('tasks', newTasks)
     }
-  
-    addTask(newTaskName){
-      const newTasks = this.tasks.concat({
-        taskName: newTaskName,
-        isCompleted: false
-      })
-  
-      this.setState('tasks', newTasks)
-    }
-  
+
     toggleCompleted(taskIndex) {
-      // can be done better using .map to create new tasks array
-      // with one task toggled
-  
-      const task = this.tasks[taskIndex]
-  
-      task.isCompleted = !task.isCompleted
-  
-      this.setState('tasks', this.tasks)
+        // can be done better using .map to create new tasks array
+        // with one task toggled
+
+        const task = this.tasks[taskIndex]
+
+        task.isCompleted = !task.isCompleted
+
+        this.setState('tasks', this.tasks)
     }
-  
+
     deleteTask(taskIndex) {
-      const newTasks = this.tasks.filter((task, index) => index !== taskIndex)
-  
-      this.setState('tasks', newTasks)
+        const newTasks = this.tasks.filter((task, index) => index !== taskIndex)
+
+        this.setState('tasks', newTasks)
     }
-  
+
     renderTask(taskName, isCompleted, taskIndex) {
-      const taskElement = document.createElement('div')
-  
-      taskElement.innerText = taskName
-  
-      if (isCompleted) {
-        taskElement.style.textDecoration = 'line-through'
-      }
-  
-      taskElement.addEventListener(
-        'click',
-        () => { this.toggleCompleted(taskIndex) }
-      )
-      taskElement.addEventListener(
-        'dblclick',
-        () => { this.deleteTask(taskIndex) }
-      )
-  
-      return taskElement
+        const taskElement = document.createElement('div')
+
+        taskElement.innerText = taskName
+
+        if (isCompleted) {
+            taskElement.style.textDecoration = 'line-through'
+        }
+
+        taskElement.addEventListener(
+            'click',
+            () => { this.toggleCompleted(taskIndex) }
+        )
+        taskElement.addEventListener(
+            'dblclick',
+            () => { this.deleteTask(taskIndex) }
+        )
+
+        return taskElement
     }
-  
+
     render() {
-      this.tasksContainer.innerText = ''
-  
-      if (this.isLoading) {
-        this.tasksContainer.innerText = 'Ładowanie...'
-        return
-      }
-  
-      if (this.tasks.length === 0) {
-        this.tasksContainer.innerText = 'Brak zadań!'
-        return
-      }
-  
-      for (let i = 0; i < this.tasks.length; i++) {
-        const task = this.tasks[i]
-        const taskName = task.taskName
-        const isCompleted = task.isCompleted
-  
-        const item = this.renderTask(taskName, isCompleted, i)
-  
-        this.tasksContainer.appendChild(item)
-      }
+        this.tasksContainer.innerText = ''
+
+        if (this.isLoading) {
+            this.tasksContainer.innerText = 'Ładowanie...'
+            return
+        }
+
+        if (this.tasks.length === 0) {
+            this.tasksContainer.innerText = 'Brak zadań!'
+            return
+        }
+
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i]
+            const taskName = task.taskName
+            const isCompleted = task.isCompleted
+
+            const item = this.renderTask(taskName, isCompleted, i)
+
+            this.tasksContainer.appendChild(item)
+        }
     }
-  }
+}
