dc701c54a3d265da57d9c0ca2fd4c81694eee26a
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-28 12:16:29 +0200

    Zmiana statusu taska po kliknieciu

--- ToDo.js
+++ ToDo.js
@@ -1,70 +1,80 @@
 class ToDo {
-    constructor(selector) {
-        this.mainContainerElement = document.querySelector(selector)
-        this.uiContainer = null
-        this.tasksContainer = null
+  constructor(selector) {
+    this.mainContainerElement = document.querySelector(selector)
+    this.uiContainer = null
+    this.tasksContainer = null
 
-        this.isLoading = false
-        this.tasks = [
-            {
-                taskName: 'Wynieś śmieci',
-                isCompleted: true
-            }
-        ]
+    this.isLoading = false
+    this.tasks = [
+      {
+        taskName: 'Wynieś śmieci',
+        isCompleted: true
+      }
+    ]
+  }
+
+  init() {
+    this.uiContainer = document.createElement('div')
+    this.tasksContainer = document.createElement('div')
+    this.mainContainerElement.appendChild(this.uiContainer)
+    this.mainContainerElement.appendChild(this.tasksContainer)
+
+    this.render()
+  }
+
+  toggleCompleted(taskIndex) {
+    const task = this.tasks[taskIndex]
+
+    task.isCompleted = ! // Odwrotnosc tego co bylo
+    this.render()
+  }
+
+  deleteTask(taskIndex) {
+    console.log(taskIndex)
+  }
+
+  renderTask(taskName, isCompleted, taskIndex) {
+    const taskElement = document.createElement('div')
+
+    taskElement.innerText = taskName
+
+    if (isCompleted) {
+      taskElement.style.textDecoration = 'line-through'
     }
 
-    init() {
-        this.uiContainer = document.createElement('div')
-        this.tasksContainer = document.createElement('div')
-        this.mainContainerElement.appendChild(this.uiContainer)
-        this.mainContainerElement.appendChild(this.tasksContainer)
+    taskElement.addEventListener(
+      'click',
+      () => { this.toggleCompleted(taskIndex) }
+    )
+    taskElement.addEventListener(
+      'dblclick',
+      () => { this.deleteTask(taskIndex) }
+    )
 
-        this.render()
+    return taskElement
+  }
+
+  render() {
+    this.tasksContainer.innerText = ''
+
+    if (this.isLoading) {
+      this.tasksContainer.innerText = 'Ładowanie...'
+      return
     }
 
-    renderTask(taskName, isCompleted) {
-        const taskElement = document.createElement('div')
-
-        taskElement.innerText = taskName
-
-        if(isCompleted) {
-            taskElement.style.textDecoration = 'line-through'
-        }
-
-        taskElement.addEventListener(
-            'click',
-            () => {}
-        )
-
-        taskElement.addEventListener(
-            'dblclick',
-            () => {}
-        )
-
-        return taskElement
+    if (this.tasks.length === 0) {
+      this.tasksContainer.innerText = 'Brak zadań!'
+      return
     }
 
-    render() {
-        this.tasksContainer.innerText = ''
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i]
+      const taskName = task.taskName
+      const isCompleted = task.isCompleted
 
-        if (this.isLoading) {
-            this.tasksContainer.innerText = 'Ładowanie...'
-            return
-        }
+      const item = this.renderTask(taskName, isCompleted, i)
 
-        if (this.tasks.length === 0) {
-            this.tasksContainer.innerText = 'Brak zadań!'
-            return
-        }
-
-        for (let i = 0; i < this.tasks.length; i++) {
-            const task = this.tasks[i]
-            const taskName = task.taskName
-            const isCompleted = task.isCompleted
-
-            const item = this.renderTask(taskName, isCompleted)
-
-            this.tasksContainer.appendChild(item)
-        }
+      this.tasksContainer.appendChild(item)
     }
+  }
 }
